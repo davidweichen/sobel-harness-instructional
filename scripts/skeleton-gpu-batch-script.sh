36a51f254309dba17be4198ff9daa99a52b774fd
davidweichen <david19990212@gmail.com>
2023-11-01 01:10:52 -0700

    add pause

--- scripts/skeleton-gpu-batch-script.sh
+++ scripts/skeleton-gpu-batch-script.sh
@@ -15,13 +15,12 @@
 # it is assumed your environment is set up properly for using the Cori GPUs
 # prior to you launching this batch script
 #
-dcgmi profile –pause
+dcgmi profile --pause
 
 for B in 1 4 16 64 256 1024 4096
    do
       for N in 32 64 128 256 512 1024  
          do
-         dcgmi profile –pause
          echo "threads per blocks=$N blocks=$B"
          ncu --set default --section SourceCounters --metrics smsp__cycles_active.avg.pct_of_peak_sustained_elapsed,dram__throughput.avg.pct_of_peak_sustained_elapsed,gpu__time_duration.avg ./sobel_gpu -N $N -B $B
          done 
