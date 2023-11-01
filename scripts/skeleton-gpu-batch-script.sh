ca4cb75c797a8c2a0c68cde5e160abcf72ca21a6
davidweichen <david19990212@gmail.com>
2023-10-31 23:21:39 -0700

    add pause

--- scripts/skeleton-gpu-batch-script.sh
+++ scripts/skeleton-gpu-batch-script.sh
@@ -22,7 +22,7 @@ for B in 1 4 16 64 256 1024 4096
       for N in 32 64 128 256 512 1024  
          do
          echo "threads per blocks=$N blocks=$B"
-         ncu --set default --section SourceCounters --metrics smsp__cycles_active.avg.pct_of_peak_sustained_elapsed,dram__throughput.avg.pct_of_peak_sustained_elapsed,gpu__time_duration.avg ./sobel_cpu_omp_offload -N $N -B $B
+         ncu --set default --section SourceCounters --metrics smsp__cycles_active.avg.pct_of_peak_sustained_elapsed,dram__throughput.avg.pct_of_peak_sustained_elapsed,gpu__time_duration.avg ./sobel_gpu -N $N -B $B
          done 
     done
 
