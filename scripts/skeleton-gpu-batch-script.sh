3e1fccdf81833c5a1590172fbdc2cc9b814ad6fa
davidweichen <david19990212@gmail.com>
2023-11-01 23:17:49 -0700

    add scripts

--- scripts/skeleton-gpu-batch-script.sh
+++ scripts/skeleton-gpu-batch-script.sh
@@ -25,7 +25,7 @@ for B in 1 4 16 64 256 1024 4096
          ncu --set default --section SourceCounters --metrics smsp__cycles_active.avg.pct_of_peak_sustained_elapsed,dram__throughput.avg.pct_of_peak_sustained_elapsed,gpu__time_duration.avg ./sobel_gpu -N $N -B $B
          done 
     done
-
+ncu --set default --section SourceCounters --metrics smsp__cycles_active.avg.pct_of_peak_sustained_elapsed,dram__throughput.avg.pct_of_peak_sustained_elapsed,gpu__time_duration.avg ./sobel_cpu_omp_offload
 
 
 
